c5110001c7849f8790f818ae1849cc96720f4a00
Jeremy <keremey57@gmail.com>
2020-10-21 00:36:06 +0300

    Card popup HTML slightly restructured and appended.

--- src/index.js
+++ src/index.js
@@ -20,8 +20,12 @@ const ICON_CALENDAR = <svg strokeWidth="6" viewBox="0 0 16 17.785"><g transform=
 const ICON_ASSIGNEES = <svg strokeWidth="6" viewBox="0 0 19.5 14.363"><g transform="translate(0.75 0.75)"><circle cx="2.77" cy="2.77" r="2.77" transform="translate(2.851)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.5"/><path d="M18.8,40.345A5.605,5.605,0,0,0,13.2,34.74h0A5.605,5.605,0,0,0,7.59,40.345Z" transform="translate(-7.59 -27.482)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.5"/><circle cx="2.231" cy="2.231" r="2.231" transform="translate(11.244 2.253)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.5"/><path d="M38.38,42.214h6.645A4.514,4.514,0,0,0,40.5,37.7h0a5.375,5.375,0,0,0-2.307.623" transform="translate(-27.025 -29.351)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.5"/></g></svg>;
 const ICON_SCHEDULLE = <svg strokeWidth="6" viewBox="0 0 17.5 18.105"><g transform="translate(0.75 0.75)"><path d="M15.8,23.609H7.127a.78.78,0,0,1-.777-.764V10.407a.777.777,0,0,1,.777-.777H19.552a.777.777,0,0,1,.777.777v6.986" transform="translate(-6.35 -8.071)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/><line x2="13.979" transform="translate(0 4.99)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/><line y1="1.559" transform="translate(3.153)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/><line y1="1.559" transform="translate(10.626)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/><circle cx="3.877" cy="3.877" r="3.877" transform="translate(8.246 8.851)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/><path d="M45.22,36.7v2.844l1.357,1.042" transform="translate(-33.097 -26.698)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5"/></g></svg>;
 const ICON_TAG = <svg strokeWidth="6" viewBox="0 0 19.5 19.345"><g transform="translate(1.75 1.75)"><g transform="translate(-7.42 -7.85)" fill="none" strokeLinecap="square"><path d="M7.42,14.558V8.165a.315.315,0,0,1,.315-.315h7.122a.331.331,0,0,1,.224.091l8.244,8.247a.315.315,0,0,1,0,.451L15.973,23.6a.315.315,0,0,1-.451,0L7.5,14.773A.337.337,0,0,1,7.42,14.558Z" stroke="none"/><path d="M 7.735389709472656 7.850000381469727 C 7.561208724975586 7.850000381469727 7.420000076293945 7.991209030151367 7.420000076293945 8.165390014648438 L 7.420000076293945 14.55834007263184 C 7.421428680419922 14.63722038269043 7.450439453125 14.71310043334961 7.501998901367188 14.7728099822998 L 15.52236938476563 23.60055923461914 C 15.58169937133789 23.6612491607666 15.66299915313721 23.69545936584473 15.74786949157715 23.69545936584473 C 15.83273887634277 23.69545936584473 15.91402912139893 23.6612491607666 15.97336959838867 23.60055923461914 L 23.32510948181152 16.6399097442627 C 23.38578796386719 16.58057975769043 23.41999816894531 16.4992790222168 23.41999816894531 16.41440963745117 C 23.41999816894531 16.32952880859375 23.38578796386719 16.24823951721191 23.32510948181152 16.18890953063965 L 15.08081912994385 7.941459655761719 C 15.02037906646729 7.883829116821289 14.94039916992188 7.851160049438477 14.85688877105713 7.850000381469727 L 7.735389709472656 7.850000381469727 M 7.735389709472656 6.100000381469727 L 14.85688877105713 6.100000381469727 L 14.86903953552246 6.100000381469727 L 14.88119888305664 6.100170135498047 C 15.407639503479 6.107479095458984 15.90741920471191 6.311599731445313 16.28845977783203 6.674930572509766 L 16.30365943908691 6.689420700073242 L 16.31848907470703 6.704259872436523 L 24.55835151672363 14.94728088378906 C 24.94723701477051 15.33273124694824 25.16999816894531 15.8666353225708 25.16999816894531 16.41440963745117 C 25.16999816894531 16.96677017211914 24.94348907470703 17.50502967834473 24.54854965209961 17.89118957519531 L 24.53849983215332 17.90102005004883 L 24.52827835083008 17.91069030761719 L 17.18978881835938 24.85879516601563 C 16.80645179748535 25.23223876953125 16.2838020324707 25.44545936584473 15.74786949157715 25.44545936584473 C 15.19544887542725 25.44545936584473 14.65714836120605 25.21891021728516 14.27098846435547 24.82390022277832 L 14.24862861633301 24.8010196685791 L 14.22711944580078 24.7773494720459 L 6.206748962402344 15.94960021972656 L 6.191898345947266 15.93325996398926 L 6.177469253540039 15.91654968261719 C 5.85923957824707 15.54800033569336 5.679109573364258 15.07691955566406 5.670289993286133 14.59005928039551 L 5.670000076293945 14.57419967651367 L 5.670000076293945 14.55834007263184 L 5.670000076293945 8.165390014648438 C 5.670000076293945 7.026529312133789 6.596529006958008 6.100000381469727 7.735389709472656 6.100000381469727 Z" stroke="none" fill="currentColor"/></g><g transform="translate(2.801 2.575)" fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.75"><circle cx="1.224" cy="1.224" r="1.224" stroke="none"/><circle cx="1.224" cy="1.224" r="2.099" fill="none"/></g></g></svg>;
-const ICON_ADD_TAG = <svg strokeWidth="6" viewBox="0 0 28 28"><g transform="translate(2 2)"><g fill="none" stroke="currentColor" strokeWidth="2"><rect width="24" height="24" stroke="none"/><rect x="-1" y="-1" width="26" height="26" fill="none"/></g><g transform="translate(4 4)"><line y2="16" transform="translate(8)" fill="none" stroke="currentColor" strokeWidth="2"/><line x2="16" transform="translate(0 8)" fill="none" stroke="currentColor" strokeWidth="2"/></g></g></svg>;
-const ICON_ADD_USER = <svg strokeWidth="6" viewBox="0 0 36 36"><g fill="none" stroke="currentColor" strokeWidth="2"><circle cx="18" cy="18" r="18" stroke="none"/><circle cx="18" cy="18" r="17" fill="none"/></g><g transform="translate(10 10)"><line y2="16" transform="translate(8)" fill="none" stroke="currentColor" strokeWidth="2"/><line x2="16" transform="translate(0 8)" fill="none" stroke="currentColor" strokeWidth="2"/></g></svg>;
+const ICON_ADD_TAG = <svg strokeWidth="6" viewBox="0 0 28 28"><g transform="translate(2 2)"><g fill="none" stroke="currentColor" strokeWidth="1.5"><rect width="24" height="24" stroke="none"/><rect x="-1" y="-1" width="26" height="26" fill="none"/></g><g transform="translate(4 4)"><line y2="16" transform="translate(8)" fill="none" stroke="currentColor" strokeWidth="1.5"/><line x2="16" transform="translate(0 8)" fill="none" stroke="currentColor" strokeWidth="1.5"/></g></g></svg>;
+const ICON_ADD_USER = <svg strokeWidth="6" viewBox="0 0 36 36"><g fill="none" stroke="currentColor" strokeWidth="1.5"><circle cx="18" cy="18" r="18" stroke="none"/><circle cx="18" cy="18" r="17" fill="none"/></g><g transform="translate(10 10)"><line y2="16" transform="translate(8)" fill="none" stroke="currentColor" strokeWidth="1.5"/><line x2="16" transform="translate(0 8)" fill="none" stroke="currentColor" strokeWidth="1.5"/></g></svg>;
+const ICON_ARROW_RIGHT = <svg viewBox="0 0 13.409 8.091"><path id="arrow-down" d="M6.53,18.86l6.279,5.956L18.53,18.86" transform="translate(-5.842 -18.134)" fill="none" stroke="currentColor" strokeWidth="1.5"/></svg>;
+const ICON_DUPLICATE = <svg viewBox="0 0 17.752 19.052"><g id="copy" transform="translate(0.875 0.875)"><rect width="13.001" height="14.124" rx="1.25" transform="translate(0 3.177)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.75"/><path d="M19.348,11.27v-.437a1.345,1.345,0,0,1,1.276-1.4h10.3a1.345,1.345,0,0,1,1.276,1.4V22.151a1.345,1.345,0,0,1-1.276,1.4h-.441" transform="translate(-16.197 -9.43)" fill="none" stroke="currentColor" strokeLinecap="square" strokeWidth="1.75"/></g></svg>;
+const ICON_SAVE_TEMPLATE = <svg viewBox="0 0 15.717 17.443"><path id="attachment" d="M25.171,17.179l-6.447,6.44a4.474,4.474,0,0,1-6.074-.163C10.9,21.7,10.605,19.2,12.306,17.444c1.813-1.875,5.526-5.526,7.506-7.506a2.953,2.953,0,0,1,4.413.025,2.928,2.928,0,0,1,0,4.286L17.2,21.3a1.672,1.672,0,0,1-2.5-.022,1.722,1.722,0,0,1,.131-2.415l6.527-6.487" transform="translate(-10.198 -8.053)" fill="none" stroke="currentColor" strokeWidth="1.5"/></svg>;
+
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -46,7 +50,7 @@ class Card extends React.Component {
     super(props);
     this.name = props.name;
     this.column = props.column;
-    this.id = window.id_current;
+    this.id = props.id;
     this.state = { // Tag React components
       tags: props.tags,
       name: props.name,
@@ -55,7 +59,8 @@ class Card extends React.Component {
       description: props.description
     };
     console.log("    Creating card " + this.name);
-    window.id_current++;
+    //this.id = window.id_current;
+    //window.id_current++;
     window.board.cards[this.id] = this;
   }
 
@@ -236,13 +241,18 @@ class DisplayedCard extends Card {
     let assignees = []
     this.state.assignees.forEach(assignee => {
       assignees.push(
-        <div class="displayed-assignee">
+        <li class="displayed-assignee">
           <img src={window.users[assignee].avatar}/>
           <span>{window.users[assignee].name}</span>
-        </div>
+        </li>
         );
     });
-    assignees.push(<li>{ICON_ADD_USER}</li>);
+    assignees.push(
+        <li id="add-assignee">
+          {ICON_ADD_USER}
+          <span>Assign</span>
+        </li>
+    );
 
     return(
 
@@ -260,22 +270,40 @@ class DisplayedCard extends Card {
             </div>
             <p>{this.state.description}</p>
           </div>
-          <div className="displayed-card-section displayed-card-tags">
-            <div className="displayed-card-section-name">
-              {ICON_TAG}<span>Tags</span>
+          <div className="displayed-card-details">
+            <div className="displayed-card-section displayed-card-tags">
+              <div className="displayed-card-section-name">
+                {ICON_TAG}<span>Tags</span>
+              </div>
+              <div class="displayed-card-tag-list">{tags}</div>
             </div>
-            <div class="displayed-card-tag-list">{tags}</div>
-          </div>
-          <div className="displayed-card-section displayed-card-assignees">
-            <div className="displayed-card-section-name">
-              {ICON_ASSIGNEES}<span>Assignees</span>
+            <div className="displayed-card-section displayed-card-assignees">
+              <div className="displayed-card-section-name">
+                {ICON_ASSIGNEES}<span>Assignees</span>
+              </div>
+              <ul>{assignees}</ul>
             </div>
-            <ul>{assignees}</ul>
+            <div className="displayed-card-options">
+              <div id="move-card-button">
+                {ICON_ARROW_RIGHT}<span>Move</span>
+              </div>
+              <div id="duplicate-card-button">
+                {ICON_DUPLICATE}<span>Duplicate</span>
+              </div>
+              <div id="save-template-card-button">
+                {ICON_SAVE_TEMPLATE}<span>Save template</span>
+              </div>
+            </div>
           </div>
           <div className="displayed-card-footer">
             <div className="displayed-card-created">
               <img src={window.users[this.props.creator].avatar} style={{right:'2.75rem'}}/>
-              <span className="displayed-card-creator">{window.users[this.props.creator].name} <span>created this card on</span> {this.props.creation_date}</span>
+              <span className="displayed-card-creator">
+                {window.users[this.props.creator].name} <span>created this card on</span><span className="number"> {this.props.creation_date}</span>
+              </span>
+            </div>
+            <div className="displayed-card-id number">
+              #{window.board.project_id}-{this.props.id}
             </div>
           </div>
         </div>
@@ -321,14 +349,14 @@ let tags = {
 var boardContainer = 
             <div>
               <Board id={0} project_id={'UHC'} name={"UHC Bugs"} tags={tags}>
-                <Column id={0} name="Reported" locked={false}>
+                <Column id={1} name="Reported" locked={false}>
                   <Card name="1" column={0} tags={[0, 3]} assignees={[1, 2]} description="a"/>
                 </Column>
-                <Column id={1} name="Confirmed">
+                <Column id={2} name="Confirmed">
                   <Card name="2" column={1} tags={[0, 1, 4]} assignees={[1]}/>
                 </Column>
               </Board>
-              <DisplayedCard name="Call a function from its name/id in order to allow arrays of functions." description={"Add an option to call functions from its name/id for more flexibility, specifically to make lists of functions. Alternatively make some kind of function object."} column={1} tags={[0, 1, 4]} assignees={[1, 3]}/>
+              <DisplayedCard id={1} name="Call a function from its name/id in order to allow arrays of functions." description={"Add an option to call functions from its name/id for more flexibility, specifically to make lists of functions. Alternatively make some kind of function object."} column={1} tags={[0, 1, 4]} assignees={[1, 3]}/>
             </div>;
 
 ReactDOM.render(
